46dc7b1a878a5f16eb8347fe14e6bfac7d2f1c86
Chandra <nikhilrai.fourteen@gmail.com>
2017-10-28 02:03:07 -0400

    Update Global.asax.cs

--- Assignment02/Global.asax.cs
+++ Assignment02/Global.asax.cs
@@ -11,6 +11,21 @@ namespace Assignment02
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            Application["user"] = 0;
+        }
+        
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["user"] = (int)Application["user"] + 1;
+            Application.UnLock();
+        }
+        
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["user"] = (int)Application["user"] - 1;
+            Application.UnLock();
         }
     }
 }
